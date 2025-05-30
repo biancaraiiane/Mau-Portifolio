dfe383eac8fcc196191473788376f47734f70f57
Bianca <bianca.raiane18@gmail.com>
2025-05-30 15:29:52 -0300

    inserting icons

---
+++ icons/about.js
@@ -0,0 +1,6 @@
+export const aboutIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">
+  <circle cx="12" cy="9" r="4"></circle>
+  <path d="M5.5 21a6.5 6.5 0 0 1 13 0v2h-13v-2z"></path>
+</svg>
+
+`;
---
+++ icons/linkedin.js
@@ -0,0 +1,12 @@
+export const linkedinIcon = `
+  <svg width="32" height="32" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="LinkedIn icon">
+    <title>LinkedIn icon</title>
+    <!-- Círculo azul como fundo -->
+    <circle cx="12" cy="12" r="12" fill="#0077B5" />
+    <!-- Ícone LinkedIn branco (só o path do IN) -->
+    <path fill="black" d="M7.337 19.225H4.668V9.001h2.669v10.224zM6 7.749a1.547 1.547 0 1 1 0-3.094
+      1.547 1.547 0 0 1 0 3.094zm13.224 11.476h-2.669v-5.478c0-1.308-.025-2.992-1.824-2.992-1.826
+      0-2.106 1.428-2.106 2.902v5.568h-2.669V9.001h2.562v1.393h.037c.357-.678 1.229-1.392
+      2.528-1.392 2.705 0 3.203 1.78 3.203 4.093v6.13z"/>
+  </svg>
+`;
